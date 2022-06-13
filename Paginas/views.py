c928e3a64f3c5d64de069ca7cf06ccf051fd29f2
mateus barbeiro <mateus_garcia01@hotmail.com>
2022-06-13 20:04:21 -0300

    CONTROLE DE ACESSO grupo de usuários

--- Paginas/views.py
+++ Paginas/views.py
@@ -2,74 +2,90 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView 
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from braces.views import GroupRequiredMixin
+
 from .models import Banco, Categoria, TipoOperacao
 class Index(TemplateView): 
 	template_name = "paginas/modelo.html"
 
-class BancoCreate(CreateView):
+class BancoCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
 	model = Banco
 	fields = ['nome', 'codigo',]
 	template_name = 'Paginas/form.html'
 	success_url = reverse_lazy('listar-banco')
+	group_required = u"Administrador"
 
-class CategoriaCreate(CreateView):
-	model = Categoria
-	fields = ['nome', 'descricao',]
-	template_name = 'Paginas/form.html'
-	success_url = reverse_lazy('listar-tipo-operacao')
 
-class TipoOperacaoCreate(CreateView):
-	model = TipoOperacao
-	fields = ['nome', 'descricao',]
-	template_name = 'Paginas/form.html'
-	success_url = reverse_lazy('listar-tipo-operacao')
-
-######################################################
-
-class BancoUpdate(UpdateView):
-	model = Banco
-	fields = ['nome', 'codigo',]
-	template_name = 'Paginas/form.html'
-	success_url = reverse_lazy('listar-banco')
-
-class CategoriaUpdate(UpdateView):
+class CategoriaCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
 	model = Categoria
 	fields = ['nome', 'descricao',]
 	template_name = 'Paginas/form.html'
 	success_url = reverse_lazy('listar-categoria')
+	group_required = u"Administrador"
 
-class TipoOperacaoUpdate(UpdateView):
+class TipoOperacaoCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
 	model = TipoOperacao
 	fields = ['nome', 'descricao',]
 	template_name = 'Paginas/form.html'
 	success_url = reverse_lazy('listar-tipo-operacao')
+	group_required = u"Administrador"
 
 ######################################################
 
-class BancoDelete(DeleteView):
+class BancoUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+	model = Banco
+	fields = ['nome', 'codigo',]
+	template_name = 'Paginas/form.html'
+	success_url = reverse_lazy('listar-banco')
+	group_required = u"Administrador"
+
+class CategoriaUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+	model = Categoria
+	fields = ['nome', 'descricao',]
+	template_name = 'Paginas/form.html'
+	success_url = reverse_lazy('listar-categoria')
+	group_required = u"Administrador"
+
+class TipoOperacaoUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+	model = TipoOperacao
+	fields = ['nome', 'descricao',]
+	template_name = 'Paginas/form.html'
+	success_url = reverse_lazy('listar-tipo-operacao')
+	group_required = u"Administrador"
+
+######################################################
+
+class BancoDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
 	model = Banco
 	template_name = 'Paginas/form-delete.html'
 	success_url = reverse_lazy('listar-banco')
+	group_required = u"Administrador"
 
-class CategoriaDelete(DeleteView):
+class CategoriaDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
 	model = Categoria
-	template_name = 'Paginas/form.html'
+	template_name = 'Paginas/form-delete.html'
 	success_url = reverse_lazy('listar-categoria')
+	group_required = u"Administrador"
 
-class TipoOperacaoDelete(DeleteView):
+class TipoOperacaoDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
 	model = TipoOperacao
-	template_name = 'Paginas/form.html'
+	template_name = 'Paginas/form-delete.html'
 	success_url = reverse_lazy('listar-tipo-operacao')
-
+	group_required = u"Administrador"
+	
 ######################################################
-class BancoList(ListView):
+class BancoList(GroupRequiredMixin, LoginRequiredMixin, ListView):
 	model = Banco
 	template_name = 'paginas/listas/banco.html'
+	group_required = u"Administrador"
 
-class CategoriaList(ListView):
+class CategoriaList(GroupRequiredMixin, LoginRequiredMixin, ListView):
 	model = Categoria
 	template_name = 'paginas/listas/categoria.html'
+	group_required = u"Administrador"
 
-class TipoOperacaoList(ListView):
+class TipoOperacaoList(GroupRequiredMixin, LoginRequiredMixin, ListView):
 	model = TipoOperacao
 	template_name = 'paginas/listas/tipo-operacao.html'
+	group_required = u"Administrador"
